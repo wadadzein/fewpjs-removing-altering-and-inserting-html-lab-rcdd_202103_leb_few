eb299935bdfec9971941f6e65594809f122181f2
wadad zein <wadad_zein@hotmail.com>
2021-04-12 17:23:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let main = document.getElementById("main");
 main.parentNode.removeChild(main);
+<<<<<<< HEAD
 let newHeader=document.createElement("h1");
 newHeader.setAttribute('id','victory');
 newHeader.innerHTML="Wadadzein is the champion"; 
 
 
+=======
+let newHeader= document.main.appendChild(h1);
+newHeader.innerHTML: 
+>>>>>>> 1a1913038099010ab88555db8c6474cffcd149c6
